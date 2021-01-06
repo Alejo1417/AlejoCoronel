1543f5616523f89ab9a63a8d0989d86f06f04638
Alejo M. Coronel <coronelalejov@gmail.com>
2021-01-06 13:13:02 -0300

    Responsive, inserccion de PDF

--- SASS/script/code.js
+++ SASS/script/code.js
@@ -11,9 +11,46 @@ function btnScroll(){
     $(this).animate({top: "78%",opacity: 1, boxShadow: "0px 3px 7px #0275d8"},200)
    }, function(){
     $(this).animate({top: "80%", opacity: .4, boxShadow: "null"})
-   },300);
+},300);
 };
 btnScroll();
+
+$canvas = $('#canvas');
+$letter = $(".boxName span");
+
+//Text in Canvas
+function blur(){
+    $.fx.step.textShadowBlur = (fx) => {
+        $(fx.elem).css({textShadow: '0 0 ' + Math.floor(fx.now) + 'px #0275d8'})};
+        $($letter).hover(function() {
+            $(this).stop().animate({textShadowBlur:50}, {duration: 500});
+    }, function(){
+        $(this).animate({textShadowBlur: null}, {duration: 1500})
+    });
+}
+blur();
+function letterMove() {
+    var text = $('.allName span');
+    text.click(function(){
+        text.toggleClass('move')
+    })
+}
+letterMove();
+//Skill animation
+function skillSVG(){
+    $('.square').hover(function(){
+        $(this).animate({
+            boxShadow: '1px 2px 4px black',
+            backgroundColor: '#9b9fa178'
+        })
+    }, function(){
+        $(this).animate({
+            boxShadow: 'null',
+            backgroundColor: 'transparent'
+        })
+    })
+};
+skillSVG();
 
 //Scroll things...............
 window.sr = new ScrollReveal();
@@ -22,33 +59,24 @@ loadh2 = {
     distance: '250px',
     easing: 'cubic-bezier(.62,-0.45,.7,1.38)', 
     origin: 'left',
-    reset: true}
+    reset: true
+}
+z = {
+    duration: 1500,
+    distance: '100px',
+    origin: 'top',
+    easing: 'cubic-bezier(.37,.26,0,1.44)',
+    interval: 300,
+    duration: 1000
+}
+canvasOptions = {
+    delay: 1500,
+    duration: 1500,
+    easing: 'ease-in-out'
+}
+//sr.reveal($letter, z);
+//sr.reveal($canvas, canvasOptions);
 
-//Canvas Load
-function callBack() { 
-    sr.reveal('#canvas', {
-        duration: 600
-    })
-}
-function showCanvas(){
-    sr.reveal('#span', {
-        delay: 900,
-        duration: 800,
-        origin: 'top',
-        distance: '1000px',
-        afterReveal: callBack
-    });
-    
-}
-function canvasLoad(){
-    sr.reveal('h1', {
-        origin: 'top',
-        distance: '70px',
-        interval: 5000,
-        easing: 'cubic-bezier(.71,-0.29,.28,1.06)',
-        afterReveal: showCanvas
-    })
-};
 // Nav Load--
 sr.reveal('#sectionNav', {
     reset: true,
@@ -86,7 +114,7 @@ function aptLoad() {
 function simbolLoad() {
     sr.reveal('.load', {
         interval: 500,
-        origin: 'bottom',
+        origin: 'top',
         distance: '70px',
         easing: 'cubic-bezier(.65,1.26,.74,1.36)',
         reset: true,
@@ -94,22 +122,6 @@ function simbolLoad() {
 }
 aptLoad();
 svgSkill();
-canvasLoad();
 simbolLoad();
 ScrollReveal().reveal('h2', loadh2);
 
-//Skill animation
-function skillSVG(){
-    $('.square').hover(function(){
-        $(this).animate({
-            boxShadow: '1px 2px 4px black',
-            backgroundColor: '#9b9fa178'
-        })
-    }, function(){
-        $(this).animate({
-            boxShadow: 'null',
-            backgroundColor: 'transparent'
-        })
-    })
-};
-skillSVG();
